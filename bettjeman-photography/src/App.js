0f1098f9803616f35991e6ad8cd2cb402b2111e0
Finn Bettjeman <fluxr3@gmail.com>
2021-11-07 22:03:11 +1300

    making the componts for the catagories

--- bettjeman-photography/src/App.js
+++ bettjeman-photography/src/App.js
@@ -1,6 +1,10 @@
 import { Header } from './header'
 import { Catagory } from './catagory'
 //import { Birds } from './birds'
+//import { Landscape} from './landscape
+//import { Other-animals } form './other-animals
+//import { Night } form './night
+//import { Plants } from './plants
 
 function App() {
   return (
