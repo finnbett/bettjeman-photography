12cad093f1884f630d7c3559c042ab76389a0a23
Finn Bettjeman <fluxr3@gmail.com>
2021-11-07 21:33:16 +1300

    making the text over the images and adding the style to home

--- bettjeman-photography/src/catagory.jsx
+++ bettjeman-photography/src/catagory.jsx
@@ -2,11 +2,28 @@ import React from "react";
 export function Catagory () {
     return (
         <>
-        <img src="images/birds/IMG_3091.jpg" alt="" />
-        <img src="images/landscape/IMG_9342.jpg" alt="" />
-        <img src="images/night/IMG_9083.jpg" alt="" />
-        <img src="images/other-animals/IMG_5562.jpg" alt="" />
-        <img src="images/plants/FLOWER 3 FINAL.jpg" alt="" />
+        <div className="container">
+            <div className="image-container">
+                <img src="images/birds/IMG_3091.jpg" alt="" />
+                <div className="bird">Birds</div>
+            </div>
+            <div className="image-container">
+                <img src="images/landscape/IMG_9342.jpg" alt="" />
+                <div className="landscape">Landscape</div>
+            </div>
+            <div className="image-container">
+                <img src="images/night/IMG_9083.jpg" alt="" />
+                <div className="night">Night</div>
+            </div>
+            <div className="image-container">
+                <img src="images/other-animals/IMG_5562.jpg" alt="" />
+                <div className="other-animals">Other Animals</div>
+            </div>
+            <div className="image-container">
+                <img src="images/plants/FLOWER 3 FINAL.jpg" alt="" />
+                <div className="plants">Plants</div>
+            </div>
+        </div>
 
 
         </>
