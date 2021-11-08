f17a643bc480df8b531fc2c19b66574549df8129
Finn Bettjeman <fluxr3@gmail.com>
2021-11-08 21:53:18 +1300

    making the landscape route

--- bettjeman-photography/src/App.js
+++ bettjeman-photography/src/App.js
@@ -2,6 +2,7 @@ import { Header } from './header'
 import { Catagory } from './catagory'
 import { Route, Routes } from "react-router";
 import  Birds  from './birds'
+import Landscape from './landscape';
 //import { Landscape} from './landscape
 //import { Other-animals } form './other-animals
 //import { Night } form './night
@@ -14,10 +15,9 @@ function App() {
       <Header/>
       <header/>
       <Routes>
-      
-      <Route path= "/" element={<Catagory/>}/>
-
-      <Route path="/birds" element= {<Birds/>}/>
+        <Route path= "/" element={<Catagory/>}/>
+        <Route path="/birds" element= {<Birds/>}/>
+        <Route path="/landscape" element= {<Landscape/>}/>
       </Routes>
     </div>
    
