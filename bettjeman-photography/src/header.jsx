8e69dfacde917b94b12104e671cdf84b6eeb4357
Finn Bettjeman <fluxr3@gmail.com>
2021-11-09 16:41:01 +1300

    created home link

--- bettjeman-photography/src/header.jsx
+++ bettjeman-photography/src/header.jsx
@@ -3,6 +3,7 @@ import React from "react";
  export function Header () {
     return (
         <>
+        <a href="/"className="homeLink">Home</a>
         <h1>Bettjeman Photography</h1>
         </>
     )
